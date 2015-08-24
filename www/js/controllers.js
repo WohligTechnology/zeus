638eb9cf6bbec9f1b8b8fc7bdb5e96252adc8ce3
Chintan Shah <chintan@wohlig.com>
2015-08-25 00:07:25 +0530

    controller done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -41,16 +41,67 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('AccessCtrl', function($scope) {
+
 })
+.controller('LoginCtrl', function($scope) {
+
+})
+.controller('ResetPasswordCtrl', function($scope) {
+
+})
+.controller('ForgotPasswordCtrl', function($scope) {
+
+})
+.controller('SignupCtrl', function($scope) {
+
+})
+.controller('HomeCtrl', function($scope) {
+
+})
+.controller('ContentPageCtrl', function($scope) {
+
+})
+.controller('EventsCtrl', function($scope) {
+
+})
+.controller('EventDetailCtrl', function($scope) {
+
+})
+    .controller('BlogsCtrl', function($scope) {
+
+})
+    .controller('BlogDetailCtrl', function($scope) {
+
+})
+    .controller('PhotoGalleryCategoryCtrl', function($scope) {
+
+})
+    .controller('PhotoGalleryCtrl', function($scope) {
+
+})
+    .controller('VideoGalleryCategoryCtrl', function($scope) {
+
+})
+    .controller('VideoGalleryCtrl', function($scope) {
+
+})
+.controller('AccountCtrl', function($scope) {
+
+})
+.controller('SettingCtrl', function($scope) {
+
+})
+.controller('NotificationCtrl', function($scope) {
+
+})
+.controller('ContactCtrl', function($scope) {
+
+})
+.controller('SearchCtrl', function($scope) {
+
+})
+
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 });
